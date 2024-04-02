4dacbb3c4eed466a173dc169af3c5f829c9edd72
tylerkeyes <tyler.keyes00@gmail.com>
2024-04-01 21:26:17 -0400

    add TTL for items in cache and remove when expired

--- dns-forward.go
+++ dns-forward.go
@@ -16,7 +16,7 @@ const (
 	CLOUDFLARE_DNS = "1.1.1.1:53"
 	GOOGLE_DNS     = "8.8.8.8:53"
 	absoluteMinTTL = 60
-	absoluteMaxTTL = 2147483647
+	absoluteMaxTTL = 86400
 )
 
 var (
@@ -25,25 +25,37 @@ var (
 	fakeAdd    = flag.String("fakead", "127.0.0.1", "an ip to send back for filtered domains")
 	upConn     = flag.String("up-conn", "udp", "upstream dns connection type <udp|tcp>")
 	listenConn = flag.String("listen-conn", "udp", "dns server connection type <udp|tcp>")
+	ttl        = flag.Int("ttl", absoluteMinTTL, "TTL for cached dns results, min of 60s, max of 1 day")
 
 	flush     = make(chan struct{})
-	dataCache = make(map[string]string)
+	dataCache = make(map[string]dnsValue)
 	dataMux   = &sync.Mutex{}
 )
 
+type dnsHandler struct{}
+
+type dnsValue struct {
+	ipAddress  string
+	storedTime time.Time
+}
+
 func main() {
 	flag.Parse()
+	if *ttl < absoluteMinTTL {
+		*ttl = absoluteMinTTL
+	} else if *ttl > absoluteMaxTTL {
+		*ttl = absoluteMaxTTL
+	}
 
 	server := &dns.Server{Addr: *listenAddr, Net: *listenConn}
 	server.Handler = &dnsHandler{}
 	log.Printf("upstream dns: %v connection %v", *upServer, *upConn)
 	log.Printf("dns listen on: %v connection %v", *listenAddr, *listenConn)
+	go cleanCache(*ttl)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("fatal: failed to set udp|tcp listener %s\n", err.Error())
 	}
 }
-
-type dnsHandler struct{}
 
 func (*dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	msg := dns.Msg{}
@@ -52,11 +64,12 @@ func (*dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	switch r.Question[0].Qtype {
 	case dns.TypeA:
 		msg.Authoritative = true
+		msg.RecursionAvailable = true
 		domain := msg.Question[0].Name
 		address, ok := checkDomain(domain)
 		if ok {
 			msg.Answer = append(msg.Answer, &dns.A{
-				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(*ttl)},
 				A:   net.ParseIP(address),
 			})
 		}
@@ -65,15 +78,14 @@ func (*dnsHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 }
 
 func checkDomain(domain string) (string, bool) {
-	for k, v := range dataCache {
-		if k == domain {
-			return v, true
-		}
+	v, ok := dataCache[domain]
+	if ok {
+		return v.ipAddress, true
 	}
 
 	if addr, ok := resolveDomain(domain); ok {
 		dataMux.Lock()
-		dataCache[domain] = addr
+		dataCache[domain] = dnsValue{ipAddress: addr, storedTime: time.Now()}
 		dataMux.Unlock()
 		return addr, true
 	}
@@ -97,4 +109,20 @@ func resolveDomain(domain string) (string, bool) {
 		return "err", false
 	}
 	return ip[0], true
+}
+
+func cleanCache(ttlSeed int) {
+	ttl := time.Duration(time.Duration(ttlSeed) * time.Second)
+	for {
+		now := time.Now()
+		time.Sleep(ttl)
+
+		for k, v := range dataCache {
+			if v.storedTime.Before(now) {
+				dataMux.Lock()
+				delete(dataCache, k)
+				dataMux.Unlock()
+			}
+		}
+	}
 }
